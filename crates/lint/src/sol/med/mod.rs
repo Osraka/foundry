a0c362da1321228294c70d48c0a51c65fc301c26
Osraka <98612432+Osraka@users.noreply.github.com>
2026-06-23 11:38:22 +0300

    Revert "feat(lint): add non-reentrant-not-first detector (#15248)"
    This reverts commit 607ea4a08ae6c77a392f7fe4435f7615c7051748.

--- crates/lint/src/sol/med/mod.rs
+++ crates/lint/src/sol/med/mod.rs
@@ -36,9 +36,6 @@ use unused_return::UNUSED_RETURN;
 mod locked_ether;
 use locked_ether::LOCKED_ETHER;
 
-mod non_reentrant_not_first;
-use non_reentrant_not_first::NON_REENTRANT_NOT_FIRST;
-
 mod tautological_compare;
 use tautological_compare::TAUTOLOGICAL_COMPARE;
 
@@ -58,7 +55,6 @@ register_lints!(
     (UnsafeTypecast, late, (UNSAFE_TYPECAST)),
     (UnusedReturn, late, (UNUSED_RETURN)),
     (LockedEther, late, (LOCKED_ETHER)),
-    (NonReentrantNotFirst, late, (NON_REENTRANT_NOT_FIRST)),
     (WeakPrng, early, (WEAK_PRNG)),
     (TautologicalCompare, late, (TAUTOLOGICAL_COMPARE))
 );
